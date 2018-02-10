13b347170d02f8f6ea0f8de4522516a9ac414415
Igor de Alcantara Barroso <idabmat@gmail.com>
2018-02-10 18:51:49 +0100

    Setup and run formatter

--- test/tic_tac_toe_channel_web/channels/game_channel_test.exs
+++ test/tic_tac_toe_channel_web/channels/game_channel_test.exs
@@ -14,55 +14,61 @@ defmodule TicTacToeChannelWeb.GameChannelTest do
   test "get game state", %{socket: socket} do
     push(socket, "new_game")
     push(socket, "get_state")
-    assert_push "game_state", %{board: board}
+    assert_push("game_state", %{board: board})
+
     assert board == [
-      [nil, nil, nil],
-      [nil, nil, nil],
-      [nil, nil, nil]
-    ]
+             [nil, nil, nil],
+             [nil, nil, nil],
+             [nil, nil, nil]
+           ]
   end
 
   test "player makes a move", %{socket: socket} do
     push(socket, "new_game")
     push(socket, "player_move", 5)
-    assert_push "game_state", %{}
+    assert_push("game_state", %{})
   end
 
   test "ask computer to move", %{socket: socket} do
     push(socket, "new_game")
     push(socket, "computer_move")
-    assert_push "game_state", %{}
+    assert_push("game_state", %{})
   end
 
   test "get a new game", %{socket: socket} do
     push(socket, "new_game", "original")
-    assert_push "game_state", %{board: board}
+    assert_push("game_state", %{board: board})
+
     assert board == [
-      [nil, nil, nil],
-      [nil, nil, nil],
-      [nil, nil, nil]
-    ]
+             [nil, nil, nil],
+             [nil, nil, nil],
+             [nil, nil, nil]
+           ]
   end
 
   test "get a new misere game", %{socket: socket} do
     push(socket, "new_game", "misere")
-    assert_push "game_state", %{board: board, game_mode: game_mode}
+    assert_push("game_state", %{board: board, game_mode: game_mode})
+
     assert board == [
-      [nil, nil, nil],
-      [nil, nil, nil],
-      [nil, nil, nil]
-    ]
+             [nil, nil, nil],
+             [nil, nil, nil],
+             [nil, nil, nil]
+           ]
+
     assert game_mode == :misere
   end
 
   test "get a new notakto game", %{socket: socket} do
     push(socket, "new_game", "notakto")
-    assert_push "game_state", %{board: board, game_mode: game_mode}
+    assert_push("game_state", %{board: board, game_mode: game_mode})
+
     assert board == [
-      [nil, nil, nil],
-      [nil, nil, nil],
-      [nil, nil, nil]
-    ]
+             [nil, nil, nil],
+             [nil, nil, nil],
+             [nil, nil, nil]
+           ]
+
     assert game_mode == :notakto
   end
 end
